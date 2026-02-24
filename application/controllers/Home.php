33061efa1aae85fd4f6ea72fcf691c7190427b4c
byprase283 <bayu.prasetio83@gmail.com>
2026-02-24 21:27:12 +0700

    produk default home 12

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,7 +18,7 @@ class Home extends CI_Controller
         $data['title'] = get_setting('site_name', 'Peweka') . ' - Culture & The Future';
         $data['categories'] = $this->Category_model->get_all();
         $data['active_category'] = $category_slug;
-        $data['products'] = $this->Product_model->get_all(TRUE, $category_slug, 8);
+        $data['products'] = $this->Product_model->get_all(TRUE, $category_slug, 12);
 
         $this->load->view('layout/header', $data);
         $this->load->view('home/index', $data);
